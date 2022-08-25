065cde6b3357a1ea736c59dd9dcae7f5e035c066
HelloHailie <alsltjsals@gmail.com>
2022-08-25 23:23:27 +0900

    feat:login page

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,44 +1,47 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-
-const Button = ({text,type,onClick}) => {
-    const types = ['red','login'].includes(type)? type:'default'
+const Button = ({ text, type, onClick }) => {
+  const types = ["red", "login"].includes(type) ? type : "default";
   return (
-    <ButtonCom className={['myButton',`myButton_${types}`].join(' ')} onClick={onClick}>{text}</ButtonCom>
-  )
-}
+    <ButtonCom
+      className={["myButton", `myButton_${types}`].join(" ")}
+      onClick={onClick}
+    >
+      {text}
+    </ButtonCom>
+  );
+};
 
 Button.defaultProps = {
-    type: "default"
-  }
-export default Button
+  type: "default",
+};
+export default Button;
 
-// 스타일드 컴포넌트 
+// 스타일드 컴포넌트
 const ButtonCom = styled.button`
-padding: 8px 13px;
-border: none;
-color: #fff;
-background-color: #1764aa;
-font-size: 14px;
-white-space: nowrap;
-border-radius: 3px;
-font-weight: 600;
-margin-left: 10px;
-cursor: pointer;
+  padding: 8px 13px;
+  border: none;
+  color: #fff;
+  background-color: #1764aa;
+  font-size: 14px;
+  white-space: nowrap;
+  border-radius: 3px;
+  font-weight: 600;
+  margin-left: 10px;
+  cursor: pointer;
 
-&:hover{
+  &:hover {
+  }
 
-}
-
-&.myButton_red{
+  &.myButton_red {
     background-color: red;
     color: #fff;
-}
+  }
 
-&.myButton_login{
-    background-color: #B3D3EA;
+  &.myButton_login {
+    background-color: #b3d3ea;
     font-weight: 500;
     border: 1px solid #1764aa;
-    color:#1764aa;
-}
-`
+    color: #1764aa;
+  }
+`;
