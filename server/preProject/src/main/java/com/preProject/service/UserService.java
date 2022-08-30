35ab5208f35e2101ea2f6e785d8bd13286c37c13
“Sohyeon” <yeahh2483@gmail.com>
2022-08-30 20:56:08 +0900

    feat: Question CRUD 추가

--- server/preProject/src/main/java/com/preProject/service/UserService.java
+++ server/preProject/src/main/java/com/preProject/service/UserService.java
@@ -4,6 +4,10 @@ import com.preProject.domain.User;
 import com.preProject.exception.BusinessLogicException;
 import com.preProject.exception.ExceptionCode;
 import com.preProject.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
@@ -25,6 +29,15 @@ public class UserService {
         verifyExistEmail(user.getEmail());
         User savedUser = userRepository.save(user);
         return savedUser;
+    }
+
+    //로그인
+    public User login(User user) {
+        User findUser = userRepository.findById(user.getId()).orElseThrow(() -> {
+            return new IllegalArgumentException("User does not exist");
+        });
+        verifyExistEmail(user.getEmail());
+        return findUser;
     }
 
     //회원 정보 수정
@@ -53,6 +66,24 @@ public class UserService {
         return findUser;
     }
 
+    //회원 목록 정렬
+    @Transactional(readOnly = true)
+//    public Page<User> findUserList(Pageable pageable) {
+//        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber()-1, pageable.getPageSize());
+//        return userRepository.findAll(pageable);
+//    }
+    public Page<User> findUserList(int page, int size) {
+        return userRepository.findAll(PageRequest.of(page, size,
+                Sort.by("id").descending()));
+
+    }
+
+    //특정 회원 정보 조회
+    @Transactional(readOnly = true)
+    public User findUserById(Long id) {
+        return findVerifiedUser(id);
+    }
+
     //회원 정보 삭제
     public void deleteUser(Long id) {
         User findUser = findVerifiedUser(id);
@@ -74,4 +105,5 @@ public class UserService {
         if (user.isPresent())
             throw new BusinessLogicException(ExceptionCode.USER_EXISTS);
     }
+
 }
