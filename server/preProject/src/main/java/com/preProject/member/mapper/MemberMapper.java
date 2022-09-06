8e08436b5428045d8023fef1e56dcca4602ed31d
Damm06 <ghkeka111@gmail.com>
2022-09-06 11:32:46 +0900

    upadte: S3 added again

--- server/preProject/src/main/java/com/preProject/member/mapper/MemberMapper.java
+++ server/preProject/src/main/java/com/preProject/member/mapper/MemberMapper.java
@@ -6,10 +6,11 @@ import com.preProject.member.dto.MemberPatchDto;
 import com.preProject.member.dto.MemberPostDto;
 import com.preProject.member.dto.MemberResponseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
     Member memberPostToMember(MemberPostDto requestBody);
 
